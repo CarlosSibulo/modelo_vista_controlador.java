14e8c3c5018f1b10e64d4a64022f772438ecd58c
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-03-17 07:58:50 -0500

    Create reservas_modulo.java

---
+++ reservas_modulo.java
@@ -0,0 +1,41 @@
+// Clase Cliente
+public class Reserva {
+    private String clienteNombre;
+    private String clienteEmail;
+
+    public Reserva(String clienteNombre, String clienteEmail) {
+        this.clienteNombre = clienteNombre;
+        this.clienteEmail = clienteEmail;
+    }
+
+    public String getClienteNombre() {
+        return clienteNombre;
+    }
+
+    public String getClienteEmail() {
+        return clienteEmail;
+    }
+}
+
+// Clase Lugar
+public class Asiento {
+    private int asientoNumero;
+    private boolean estaReservado;
+
+    public Asiento(int asientoNumero) {
+        this.asientoNumero = asientoNumero;
+        this.estaReservado = false;
+    }
+
+    public int getAsientoNumero() {
+        return asientoNumero;
+    }
+
+    public boolean isEstaReservado() {
+        return estaReservado;
+    }
+
+    public void reservar() {
+        this.estaReservado = true;
+    }
+}
