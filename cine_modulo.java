3d92d637020a148a07753291cfc6b1273b1e18ce
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-03-17 08:02:53 -0500

    Update cine_modulo.java

--- cine_modulo.java
+++ cine_modulo.java
@@ -1,18 +1,43 @@
-public class fizzbus_modulo {
-    public class Juego_FizzBuzz {
-    public class FizzBuzz {
-    public String obtenerResultado(int cantidad) {
-        if (cantidad % 3 == 0 && cantidad % 5 == 0) {
-            return "FizzBuzz";
-        } else if (cantidad % 3 == 0) {
-            return "Fizz";
-        } else if (cantidad % 5 == 0) {
-            return "Buzz";
-             } else {
-            return Integer.toString(cantidad);
-        }
+// Clase Cliente
+public class Reserva {
+    private String clienteNombre;
+    private String clienteEmail;
+
+    public Reserva(String clienteNombre, String clienteEmail) {
+        this.clienteNombre = clienteNombre;
+        this.clienteEmail = clienteEmail;
     }
+
+    public String getClienteNombre() {
+        return clienteNombre;
     }
-    } 
+
+    public String getClienteEmail() {
+        return clienteEmail;
+    }
 }
+
+// Clase Lugar
+public class Asiento {
+    private int asientoNumero;
+    private boolean estaReservado;
+
+    public Asiento(int asientoNumero) {
+        this.asientoNumero = asientoNumero;
+        this.estaReservado = false;
+    }
+
+    public int getAsientoNumero() {
+        return asientoNumero;
+    }
+
+    public boolean isEstaReservado() {
+        return estaReservado;
+    }
+
+    public void reservar() {
+        this.estaReservado = true;
+    }
+}
+
     
